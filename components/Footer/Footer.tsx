379baf4572bd78cb12fc7a076dcbd3b7479a787c
Jason Cohen <jason.cohen@wpengine.com>
2020-07-01 11:05:30 -0500

    small change to test deploy

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -17,7 +17,7 @@ export const Footer = (p:MyProps) => {
         © 2010–2020 WP Engine, Inc. All Rights Reserved.
       </span>
       <span className={`${styles.legal} fe`}>
-        WP ENGINE®, TORQUE®, EVERCACHE®, and the cog logo service marks are owned by WP Engine, Inc.<br />
+        WP ENGINE®, TORQUE®, EVERCACHE®, and the cog logo service marks are owned by WP Engine Inc.<br />
         Some icons made by <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a>.
       </span>
     </div>
