6ae951e3333e4a8441da98e37ee531bffeca0624
Jason Cohen <jason.cohen@wpengine.com>
2020-07-01 11:33:27 -0500

    another comma to see the change

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -14,7 +14,7 @@ export const Footer = (p:MyProps) => {
     </div>
     <div className={styles.legalContainer}>
       <span className={`${styles.copyright}`}>
-        © 2010–2020 WP Engine, Inc. All Rights Reserved.
+        © 2010–2020 WP Engine Inc. All Rights Reserved.
       </span>
       <span className={`${styles.legal} fe`}>
         WP ENGINE®, TORQUE®, EVERCACHE®, and the cog logo service marks are owned by WP Engine Inc.<br />
