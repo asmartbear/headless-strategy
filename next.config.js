6bf6ef3f8e2482f9dbfa1b05abad71fd653e19fc
Jason Cohen <jason.cohen@wpengine.com>
2020-07-02 09:21:06 -0500

    iteration for auth

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ module.exports = () => {
     env: {
       WP_URL: process.env.WP_URL || "https://strategyprod.wpengine.com",
       BASIC_AUTH: !! process.env.BASIC_AUTH_SECRET,
+      GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+      GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+      SITE: process.env.SITE,
     },
   };
 };
